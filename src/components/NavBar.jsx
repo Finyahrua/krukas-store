c8e1970f863101106a70514555591bb1339c25cf
Finyahrua <finaddy@gmail.com>
2022-10-15 08:45:21 +0300

    done with the navbar comonent

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-md mt-1">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl normal-case">Krukas_Store</a>
+      </div>
+      <div className="flex-none">
+        <div className="dropdown-end dropdown">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge indicator-item badge-sm bg-red-400 border-none">8</span>
+            </div>
+          </label>
+          <div
+            tabIndex={0}
+            className="card dropdown-content card-compact mt-3 w-52 bg-base-100 shadow"
+          >
+            <div className="card-body">
+              <span className="text-lg font-bold">8 Items</span>
+              <span className="text-info">Subtotal: $999</span>
+              <div className="card-actions">
+                <button className="btn btn-primary btn-block">View cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="dropdown-end dropdown flex">
+          <div tabIndex={0} className="flex cursor-pointer flex-col px-4">
+            <h2 className="text-[15px]">John Doe</h2>
+            <p className="text-end text-[13px] font-thin">User</p>
+          </div>
+          <label tabIndex={0} className="avatar btn btn-ghost btn-circle">
+            <div className="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
